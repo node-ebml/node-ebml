044106653fa13362c1cc7fc2ea38b71b47f371cc
Manuel Wiedenmann <manuel@funkensturm.de>
2014-01-04 14:10:09 +0100

    removes emit options

--- example.js
+++ example.js
@@ -1,14 +1,13 @@
 var ebml = require('./index.js')
 var fs = require('fs')
 
-var decoder = new ebml.Decoder({
-    emitTags: ['Cluster']
-});
+var decoder = new ebml.Decoder({});
+
 decoder.on('Cluster', function(data) {
-    console.log('tag', data)
+    console.log(data.name, data)
 })
 decoder.on('Cluster:end', function(data) {
-    console.log('tag:end', data)
+    console.log(data.name + ':end', data)
 })
 
 fs.readFile('media/test.webm', function (err, data) {
