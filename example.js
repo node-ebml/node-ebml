c679ec7b24abc73664e2d9eef38ac031e5a56bf0
Oliver Walzer <walzer@incuray.com>
2015-09-26 15:04:21 +0200

    fix example after API change

--- example.js
+++ example.js
@@ -3,11 +3,8 @@ var fs = require('fs');
 
 var decoder = new ebml.Decoder();
 
-decoder.on('Cluster', function(data) {
-    console.log(data.name, data);
-});
-decoder.on('Cluster:end', function(data) {
-    console.log(data.name + ':end', data);
+decoder.on('data', function(chunk) {
+    console.log(chunk);
 });
 
 fs.readFile('media/test.webm', function(err, data) {
