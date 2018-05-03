3be9708dd2a7bcba2552704579dc174d6ec9c27d
OllieJones <olliejones@gmail.com>
2018-05-03 17:38:33 -0400

    Add tests for retrieving values from .webm stream, and fix bugs.
    There's a test file value.js and two media files it uses.
    
    A change to .eslintrc.yml is reverted.
    Buffer.from() is now used correctly.

--- streamExample.js
+++ streamExample.js
@@ -4,12 +4,12 @@ var counts = {};
 require('fs').createReadStream('media/test.webm').
     pipe(ebmlDecoder).
     on('data', function(chunk) {
-        var name = chunk[1].name;
-        if (!counts[name]) {
-            counts[name] = 0;
-        }
-        counts[name] += 1;
+    var name = chunk[1].name;
+    if (!counts[name]) {
+        counts[name] = 0;
+    }
+    counts[name] += 1;
     }).
     on('finish', function() {
-        console.log(counts);
-    });
+    console.log(counts);
+});
---
+++ test/value.js
@@ -0,0 +1,244 @@
+/* eslint-disable no-sync */
+/* globals describe, it */
+
+var fs = require('fs');
+var ebml = require('../lib/ebml/index.js'),
+    assert = require('assert');
+
+process.setMaxListeners(Infinity);
+
+describe('embl', function() {
+    describe('Values in tags', function() {
+
+        var data = fs.readFileSync('media/video-webm-codecs-avc1-42E01E.webm');
+
+        it('should get a correct PixelWidth value from a video/webm; codecs="avc1.42E01E" file (2-byte unsigned int)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'PixelWidth') {
+                    assert.equal(chunk[1].value, 352);
+                    done();
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct EBMLVersion value from a video/webm; codecs="avc1.42E01E" file (one-byte unsigned int)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'EBMLVersion') {
+                    assert.equal(chunk[1].value, 1);
+                    done();
+                }
+                decoder.on('finish', function() {
+                    assert.equal(0, 1, 'hit end of file without finding tag.');
+                    done();
+                });
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct TimeCodeScale value from a video/webm; codecs="avc1.42E01E" file (3-byte unsigned int)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'TimecodeScale') {
+                    assert.equal(chunk[1].value, 1000000);
+                    done();
+                }
+                decoder.on('finish', function() {
+                    assert.equal(0, 1, 'hit end of file without finding tag.');
+                    done();
+                });
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct TrackUID value from a video/webm; codecs="avc1.42E01E" file (56-bit integer in hex)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'TrackUID') {
+                    assert.equal(chunk[1].value, '306d02aaa74d06');
+                    done();
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct DocType value from a video/webm; codecs="avc1.42E01E" file (ASCII text)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'DocType') {
+                    assert.equal(chunk[1].value, 'webm');
+                    done();
+                }
+                decoder.on('finish', function() {
+                    assert.equal(0, 1, 'hit end of file without finding tag.');
+                    done();
+                });
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct MuxingApp value from a video/webm; codecs="avc1.42E01E" file (utf8 text)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'MuxingApp') {
+                    assert.equal(chunk[1].value, 'Chrome');
+                    done();
+                }
+                decoder.on('finish', function() {
+                    assert.equal(0, 1, 'hit end of file without finding tag.');
+                    done();
+                });
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct SimpleBlock time payload from a video/webm; codecs="avc1.42E01E" file (binary)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'SimpleBlock') {
+                    if(chunk[1].value > 0) {
+
+                        /* look at second simpleBlock */
+                        assert.equal(chunk[1].payload.byteLength, 43, 'payload length');
+                        assert.equal(chunk[1].track, 1, 'track');
+                        assert.equal(chunk[1].value, 96, 'value (timestamp)');
+                        done();
+                    }
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+
+        /* vP8 */
+        data = fs.readFileSync('media/video-webm-codecs-vp8.webm');
+
+
+        it('should get a correct PixelWidth value from a video/webm; codecs="vp8" file (2-byte unsigned int)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'PixelWidth') {
+                    assert.equal(chunk[1].value, 352);
+                    done();
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct EBMLVersion value from a video/webm; codecs="vp8" file (one-byte unsigned int)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'EBMLVersion') {
+                    assert.equal(chunk[1].value, 1);
+                    done();
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct TimeCodeScale value from a video/webm; codecs="vp8" file (3-byte unsigned int)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'TimecodeScale') {
+                    assert.equal(chunk[1].value, 1000000);
+                    done();
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct TrackUID value from a video/webm; codecs="vp8" file (56-bit integer in hex)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'TrackUID') {
+                    assert.equal(chunk[1].value, '306d02aaa74d06');
+                    done();
+                }
+                decoder.on('finish', function() {
+                    assert.equal(0, 1, 'hit end of file without finding tag.');
+                    done();
+                });
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct DocType value from a video/webm; codecs="vp8" file (ASCII text)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'DocType') {
+                    assert.equal(chunk[1].value, 'webm');
+                    done();
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+        it('should get a correct MuxingApp value from a video/webm; codecs="vp8" file (utf8 text)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'MuxingApp') {
+                    assert.equal(chunk[1].value, 'Chrome');
+                    done();
+                }
+            });
+            decoder.write(data);
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+        });
+
+        it('should get a correct SimpleBlock time payload rom a video/webm; codecs="vp8" file (binary)', function(done) {
+            var decoder = new ebml.Decoder();
+            decoder.on('data', function(chunk) {
+                if(chunk[0] === 'tag' && chunk[1].name === 'SimpleBlock') {
+                    if(chunk[1].value > 0) {
+
+                        /* look at second simpleBlock */
+                        assert.equal(chunk[1].payload.byteLength, 43, 'payload length');
+                        assert.equal(chunk[1].track, 1, 'track');
+                        assert.equal(chunk[1].value, 96, 'timestamp');
+                        assert.equal(chunk[1].discardable, false, 'discardable');
+                        done();
+                    }
+                }
+            });
+            decoder.on('finish', function() {
+                assert.equal(0, 1, 'hit end of file without finding tag.');
+                done();
+            });
+            decoder.write(data);
+        });
+
+    });
+});
