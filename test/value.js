40809efafbc418e9a0ad34c15064cca663cc1b27
OllieJones <olliejones@gmail.com>
2018-05-03 18:14:35 -0400

    Add test file, fix eslint space issues.

--- test/value.js
+++ test/value.js
@@ -15,7 +15,7 @@ describe('embl', function() {
         it('should get a correct PixelWidth value from a video/webm; codecs="avc1.42E01E" file (2-byte unsigned int)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'PixelWidth') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'PixelWidth') {
                     assert.equal(chunk[1].value, 352);
                     done();
                 }
@@ -30,7 +30,7 @@ describe('embl', function() {
         it('should get a correct EBMLVersion value from a video/webm; codecs="avc1.42E01E" file (one-byte unsigned int)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'EBMLVersion') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'EBMLVersion') {
                     assert.equal(chunk[1].value, 1);
                     done();
                 }
@@ -45,7 +45,7 @@ describe('embl', function() {
         it('should get a correct TimeCodeScale value from a video/webm; codecs="avc1.42E01E" file (3-byte unsigned int)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'TimecodeScale') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'TimecodeScale') {
                     assert.equal(chunk[1].value, 1000000);
                     done();
                 }
@@ -60,7 +60,7 @@ describe('embl', function() {
         it('should get a correct TrackUID value from a video/webm; codecs="avc1.42E01E" file (56-bit integer in hex)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'TrackUID') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'TrackUID') {
                     assert.equal(chunk[1].value, '306d02aaa74d06');
                     done();
                 }
@@ -75,7 +75,7 @@ describe('embl', function() {
         it('should get a correct DocType value from a video/webm; codecs="avc1.42E01E" file (ASCII text)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'DocType') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'DocType') {
                     assert.equal(chunk[1].value, 'webm');
                     done();
                 }
@@ -90,7 +90,7 @@ describe('embl', function() {
         it('should get a correct MuxingApp value from a video/webm; codecs="avc1.42E01E" file (utf8 text)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'MuxingApp') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'MuxingApp') {
                     assert.equal(chunk[1].value, 'Chrome');
                     done();
                 }
@@ -105,8 +105,8 @@ describe('embl', function() {
         it('should get a correct SimpleBlock time payload from a video/webm; codecs="avc1.42E01E" file (binary)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'SimpleBlock') {
-                    if(chunk[1].value > 0) {
+                if (chunk[0] === 'tag' && chunk[1].name === 'SimpleBlock') {
+                    if (chunk[1].value > 0) {
 
                         /* look at second simpleBlock */
                         assert.equal(chunk[1].payload.byteLength, 43, 'payload length');
@@ -131,7 +131,7 @@ describe('embl', function() {
         it('should get a correct PixelWidth value from a video/webm; codecs="vp8" file (2-byte unsigned int)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'PixelWidth') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'PixelWidth') {
                     assert.equal(chunk[1].value, 352);
                     done();
                 }
@@ -146,7 +146,7 @@ describe('embl', function() {
         it('should get a correct EBMLVersion value from a video/webm; codecs="vp8" file (one-byte unsigned int)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'EBMLVersion') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'EBMLVersion') {
                     assert.equal(chunk[1].value, 1);
                     done();
                 }
@@ -161,7 +161,7 @@ describe('embl', function() {
         it('should get a correct TimeCodeScale value from a video/webm; codecs="vp8" file (3-byte unsigned int)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'TimecodeScale') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'TimecodeScale') {
                     assert.equal(chunk[1].value, 1000000);
                     done();
                 }
@@ -176,7 +176,7 @@ describe('embl', function() {
         it('should get a correct TrackUID value from a video/webm; codecs="vp8" file (56-bit integer in hex)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'TrackUID') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'TrackUID') {
                     assert.equal(chunk[1].value, '306d02aaa74d06');
                     done();
                 }
@@ -191,7 +191,7 @@ describe('embl', function() {
         it('should get a correct DocType value from a video/webm; codecs="vp8" file (ASCII text)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'DocType') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'DocType') {
                     assert.equal(chunk[1].value, 'webm');
                     done();
                 }
@@ -206,7 +206,7 @@ describe('embl', function() {
         it('should get a correct MuxingApp value from a video/webm; codecs="vp8" file (utf8 text)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'MuxingApp') {
+                if (chunk[0] === 'tag' && chunk[1].name === 'MuxingApp') {
                     assert.equal(chunk[1].value, 'Chrome');
                     done();
                 }
@@ -221,8 +221,8 @@ describe('embl', function() {
         it('should get a correct SimpleBlock time payload rom a video/webm; codecs="vp8" file (binary)', function(done) {
             var decoder = new ebml.Decoder();
             decoder.on('data', function(chunk) {
-                if(chunk[0] === 'tag' && chunk[1].name === 'SimpleBlock') {
-                    if(chunk[1].value > 0) {
+                if (chunk[0] === 'tag' && chunk[1].name === 'SimpleBlock') {
+                    if (chunk[1].value > 0) {
 
                         /* look at second simpleBlock */
                         assert.equal(chunk[1].payload.byteLength, 43, 'payload length');
