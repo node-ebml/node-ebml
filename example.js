73e999ba89d1937e9925c2cbc929907342f752bf
codefactor-io <support@codefactor.io>
2018-09-05 22:57:45 +0000

    [CodeFactor] Apply fixes to commit 9d2b647
    [ci skip] [skip ci]

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
 /* eslint no-console:off */
-const fs = require('fs');
-const { Decoder } = require('./lib/ebml.js');
+const fs = require("fs");
+const { Decoder } = require("./lib/ebml.js");
 
 const decoder = new Decoder();
 
-decoder.on('data', chunk => console.log(chunk));
+decoder.on("data", chunk => console.log(chunk));
 
-fs.readFile('media/test.webm', (err, data) => {
-    if (err) {
-        throw err;
-    }
-    decoder.write(data);
+fs.readFile("media/test.webm", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  decoder.write(data);
 });
